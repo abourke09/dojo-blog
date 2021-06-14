3b1dec2f937d35fbf4b1854bc4be2e8bb3ecae29
anniebourke <abourke@revacomm.com>
2021-06-14 09:04:20 -1000

    net ninja full react tutorial #1 through #10

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import {useState} from "react";
+
+const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My New Website', body: 'Lorem ipsum uno...', author: 'Mario', id: 1 },
+        { title: 'Welcome Party!', body: 'Lorem ipsum dos...', author: 'Yoshi', id: 2 },
+        { title: 'Web Dev Top Tips', body: 'Lorem ipsum tres...', author: 'Princess P', id: 3 }
+    ])
+
+    return (
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title}</h2>
+                    <p>Written by: { blog.author }</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Home;
