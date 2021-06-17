077ff0c4b2e0889e4ce4df2677e467a07e046372
anniebourke <abourke@revacomm.com>
2021-06-16 16:38:34 -1000

    adds react router, blog details page, new blog form page, css styles

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,5 @@
 
 A
 public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
-9
-	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,16 @@
-import {useState} from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState([
-        { title: 'My New Website', body: 'Lorem ipsum uno...', author: 'Mario', id: 1 },
-        { title: 'Welcome Party!', body: 'Lorem ipsum dos...', author: 'Yoshi', id: 2 },
-        { title: 'Web Dev Top Tips', body: 'Lorem ipsum tres...', author: 'Princess P', id: 3 }
-    ])
+    const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title}</h2>
-                    <p>Written by: { blog.author }</p>
-                </div>
-            ))}
+            { error && <div>{ error }</div> }
+            { isPending && <div>...Loading</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
-};
+}
 
 export default Home;
